e1e5d6e3e9e1408002a31f7a1a4d61609171c96c
Daniel Duersteler <daniel.duersteler@gmail.com>
2020-11-13 18:19:17 +0100

    updated config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,4 @@
 <?php
-
+define('DB_CONN', 'mysql:host=localhost;dbname=playground_pdo;charset=utf8mb4');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_CONN', 'mysql:host=localhost;dbname=playground_pdo;charset=utf8mb4');
-
-ini_set('display_errors', 1);
